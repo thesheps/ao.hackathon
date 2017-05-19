cd159f4756fc5d4b9bf0b696e40543844d0e7fc3
shepc <chris.shepherd@ao.com>
2017-05-19 12:47:36 +0100

    Starting to move to material design

---
+++ app.js
@@ -0,0 +1,11 @@
+﻿var app = angular.module("aoMetrics", ["chart.js", "ngAria", "ngAnimate", "ngMaterial", "md.data.table"]);
+
+app.config(function ($mdDateLocaleProvider) {
+    $mdDateLocaleProvider.formatDate = function (date) {
+        return moment(date).format("DD/MM/YYYY");
+    };
+});
+
+app.run(function($rootScope) {
+    $rootScope.dialogs = 0;
+});
